a3fd052caa864aa9873c361e607e3edd3b262653
John Smith <iinexerjr@gmail.com>
2025-07-18 11:28:12 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+     screens: {
+      'mx' : {max: '510px'}
+     } 
+    },
+  },
+  plugins: [],
+}
+
